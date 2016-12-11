30ac6fa2def76330ae39f1ddcc718a440aa8473b
Rohrlaf <rohrlaf@mailbox.org>
2016-12-11 13:03:30 +0100

    Add local server

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,19 @@
+'use strict';
+let express  = require('express');
+let path = require('path');
+let open = require('open');
+
+const port = 3000;
+let app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
