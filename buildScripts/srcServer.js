0b37776b2b749ede330c607c35534f6657e320ce
Rohrlaf <rohrlaf@mailbox.org>
2016-12-11 22:00:41 +0100

    Add babel build task

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,17 +1,16 @@
-'use strict';
-let express  = require('express');
-let path = require('path');
-let open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
 const port = 3000;
-let app = express();
+const app = express();
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
-  if(err) {
+app.listen(port, (err) => {
+  if (err) {
     console.log(err);
   } else {
     open(`http://localhost:${port}`);
